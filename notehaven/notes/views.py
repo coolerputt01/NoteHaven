0734e6997ea4c3e0526badb75666762edf360a3a
coolerputt01 <olumideadekolu9@gmail.com>
2025-01-03 08:14:49 +0000

    Fifth

--- notehaven/notes/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request){
-    return render(request,'/workspaces/NoteHaven/notehaven/notes/templates/notes/notehaven/public/index.html',{})
-}
