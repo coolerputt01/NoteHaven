e32d3cd621fa1a90dbc99c7f8c1eea21dac2eb26
coolerputt01 <olumideadekolu9@gmail.com>
2025-01-03 06:25:52 +0000

    Fourth

--- notehaven/notes/views.py
+++ notehaven/notes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request){
+    return render(request,'/workspaces/NoteHaven/notehaven/notes/templates/notes/notehaven/public/index.html',{})
+}
