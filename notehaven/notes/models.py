2e7670d574ec2642518fad57bf3fa438e6d2cc9d
coolerputt01 <olumideadekolu9@gmail.com>
2025-01-14 16:42:35 +0000

    third

--- notehaven/notes/models.py
+++ notehaven/notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link each note to a user
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
